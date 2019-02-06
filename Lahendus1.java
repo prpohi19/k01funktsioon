400b3ae055314e60c52b410b60bdfe98ab53ca78
Kristjan Põldmets <43170909+Krispol@users.noreply.github.com>
2019-02-06 14:57:44 +0200

    Update Lahendus1.java

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,2 +1,2 @@
 //Siia tuleb esimese kodutöö lahendus
-//Kavatsen luua funktsiooni mootorite kubatuuri võimsuste suhete võrdlemiseks
+//Kavatsen luua funktsiooni mootorite kubatuuri võimsuste suhete arvutamiseks
