86a792c71a3465c5cf4039704f84ad6e59b978e5
kuntervert <43129473+kuntervert@users.noreply.github.com>
2019-02-13 01:50:58 +0200

    Update Lahendus1.java

--- Lahendus1.java
+++ Lahendus1.java
@@ -1,1 +1,33 @@
 //Esimene kodutöö//
+
+import java.util.Scanner;
+public class Algarv
+{
+    public static void main(String args[])
+    {       
+        int j, x, flag = 0;
+        System.out.print("Sisesta number:");
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        for( j = 2; j < x; j++)
+        {
+            if(x % j == 0)
+            {
+                flag = 0;
+                break;
+            }
+            else
+            {
+                flag = 1;
+            }
+         }
+         if(flag == 1)
+         {
+             System.out.println(""+x+" on algarv");
+         }
+         else
+         {
+             System.out.println(""+x+" ei ole argarv");
+         }           
+    }
+}
