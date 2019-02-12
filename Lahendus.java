ce2d96f28ef3ffd89a5e490d0adbeeb8f3c295b1
Steven Saluri <43164747+stevsal@users.noreply.github.com>
2019-02-13 00:34:27 +0200

    Add files via upload

--- Lahendus.java
+++ Lahendus.java
@@ -1,2 +1,19 @@
-//Siia tuleb esimene kodutöö lahendus
-//Teema on selgumisel.
+import java.util.Scanner;
+public class Lahendus{
+    public static void main(String[] arg){
+
+        int pudelid=3;
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Sisesta joogipudelite arv mille oled 2ra joonud: ");
+        int joodudpudelid = reader.nextInt();
+        reader.close();
+        if (pudelid<=joodudpudelid){
+            System.out.println("Mine poodi joogi j2rgi.");
+
+        } else {
+            int alles = pudelid - joodudpudelid;
+            System.out.println("Sul on alles "+alles+" pudelit jooki.");
+        }
+    }
+}
