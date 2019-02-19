dbd4bdc1ca6cbded704ff18ec01a30e84f96baa1
Krispol <krispol@users.noreply.github.com>
2019-02-20 01:52:29 +0200

    1. kodutöö üleslaadimine

---
+++ Kraadid.java
@@ -0,0 +1,18 @@
+// Esimene kodutöö
+//Funktsioon temperatuuride convertimiseks
+
+public class Kraadid {
+	public static void main(String[] args) {
+    		System.out.println("0*C on Fahrenheitis: "+CelToFar(0)+"*F");
+    		System.out.println(" ");
+    		System.out.println("32*F on Celsisuses: "+FarToCel(32)+"*C");
+
+    	}
+    	public static double CelToFar(double cel) {
+				return (cel * 9/5) + 32;
+			}
+    	public static double FarToCel(double far) {
+				return (far - 32) * 5/9;
+			}
+
+    }
